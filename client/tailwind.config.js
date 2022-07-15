92ee9390451d401e06dc358e68ffca6b71bb5cee
fredrikjohanssonnn <fjohansson.creative@gmail.com>
2022-07-15 14:52:18 +0200

    Updated the tailwind-config with some own colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,29 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Outfit', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        'primary': {
+          100: '#1F3641',
+          200: '#1A2A33'
+        },
+        'secondary': {
+          100: '#65E9E4',
+          200: '#31C3BD'
+        },
+        'tertiary': {
+          100: '#FFC860',
+          200: '#F2B137'
+        },
+        'quaternary': {
+          100: '#DBE8ED',
+          200: '#A8BFC9'
+        }
+      },
+    },
   },
   plugins: [],
 }
