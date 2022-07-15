11cdcbdc6ecaf85160fd2c93f46dcecf5b863f21
fredrikjohanssonnn <fjohansson.creative@gmail.com>
2022-07-15 14:49:44 +0200

    Added a player state. Also added a handleSquare function to map through the boards index, then check it against the index of the current square we choose. We then update the board if that index hasnt been updated yet.

--- client/src/components/Board.js
+++ client/src/components/Board.js
@@ -1,19 +1,35 @@
-import React from 'react'
+import { useState } from 'react'
 import Square from './Square'
+import { patterns } from '../patterns'
 
-const Board = ({ board }) => {
-  console.log(board)
+const Board = ({ board, setBoard }) => {
+
+  const [player, setPlayer] = useState('X')
+
+  const handleSquare = (square) => {
+    setBoard(
+      board.map((value, index) => {
+        if (index === square){
+          return player
+        }
+  
+        return value
+      })
+    )
+
+    if (player === 'X'){
+      setPlayer('O')
+    } else {
+      setPlayer('X')
+    }
+  }
+
   return (
     <div className="grid grid-cols-3 gap-4">
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
+      {/* Should be able to use index as a key. Since the index never change (no unmounting). */}
+      {board.map((_, index) => (
+        <Square value={board[index]} handleSquare={() => handleSquare(index)} key={index} />
+      ))}
     </div>
   )
 }
