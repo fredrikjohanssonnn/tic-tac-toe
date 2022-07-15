85b4ed03c1e918dfaaea95a962bddfa1bdbb7f8b
fredrikjohanssonnn <fjohansson.creative@gmail.com>
2022-07-15 14:51:51 +0200

    Updated props and the UI to subscribe for changes to the value prop. Then render the correct component based of the value. If the value is X then render the Cross-component etc.

--- client/src/components/Square.js
+++ client/src/components/Square.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import Cross from "./Cross"
+import Circle from './Circle'
 
-const Square = () => {
+const Square = ({ value, handleSquare }) => {
   return (
-    <button className="bg-cyan-800 w-24 h-24 rounded-md drop-shadow-md">
-        
+    <button onClick={handleSquare} className="flex justify-center text-quaternary-100 items-center bg-primary-100 w-24 h-24 rounded-md drop-shadow-md">
+      {value === 'X' ? <Cross /> : null}
+      {value === 'O' ? <Circle /> : null}
     </button>
   )
 }
